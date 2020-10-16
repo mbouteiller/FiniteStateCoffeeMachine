e6c9fcdc05c76e6811183f5d1a19be0288fe62e7
Martin <martin.bouteiller@etu.unice.fr>
2020-10-16 09:10:38 +0200

    Create interface implementation class

---
+++ DrinkingFactory/fr.univcotedazur.polytech.si4.fsm.project/src/fr/univcotedazur/polytech/si4/fsm/project/CoffeeMachineControlerInterfaceImplementation.java
@@ -0,0 +1,51 @@
+package fr.univcotedazur.polytech.si4.fsm.project;
+
+import fr.univcotedazur.polytech.si4.fsm.project.basiccoffeecontroller.IBasicCoffeeControllerStatemachine.SCInterfaceListener;
+
+public class CoffeeMachineControlerInterfaceImplementation implements SCInterfaceListener {
+
+    DrinkFactoryMachine theDfm;
+    public CoffeeMachineControlerInterfaceImplementation(DrinkFactoryMachine dfm) {
+        theDfm = dfm;
+    }
+    
+    @Override
+    public void onPayRaised() {
+
+    }
+
+    @Override
+    public void onRefundRaised() {
+
+    }
+
+    @Override
+    public void onGiveChangeRaised() {
+
+    }
+
+    @Override
+    public void onCheckAmountRaised() {
+
+    }
+
+    @Override
+    public void onCancelRaised() {
+
+    }
+
+    @Override
+    public void onStartRecipeRaised() {
+
+    }
+
+    @Override
+    public void onCleanRaised() {
+
+    }
+
+    @Override
+    public void onRestartRaised() {
+
+    }
+}
